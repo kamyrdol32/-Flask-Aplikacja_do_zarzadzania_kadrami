cb0e93a110e246a6288948583e6b7ac7285b9e93
kamyrdol32 <kamyrdol32@gmail.com>
2020-10-28 18:53:42 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+import functools
+
+from flask import Flask, render_template, redirect, session, jsonify, request
+
+app = Flask(__name__)
+
+### DECORATOR ###
+
+def protected(func):
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
+        if "isLogged" not in session:
+            return redirect("/login")
+        return func(*args, **kwargs)
+
+    return secure_function
+
+### INDEX ###
+
+@app.route('/')
+@protected
+def index():
+    return render_template("index.html")
+
+### LOGOWANIE I REJESTRACJA ###
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        login_mail = request.form.get('login_mail', "", type=str)
+        login_password = request.form.get('login_password', "", type=str)
+        print(login_mail)
+        print(login_password)
+        if login_mail == "admin" and login_password == "admin":
+
+            session['isLogged'] = True
+            print("Udalo sie")
+
+            return jsonify({"redirect": "/"})
+        else:
+            return jsonify({"title": "", "message": "Proszę wprowadzić poprawne dane!", "type": "danger"})
+
+    return render_template("login.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('isLogged', None)
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run()
