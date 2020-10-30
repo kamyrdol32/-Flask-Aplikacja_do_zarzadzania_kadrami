c570a61a22ab53b144f0b9d97e674021fcee087d
kamyrdol32 <kamyrdol32@gmail.com>
2020-10-30 20:30:16 +0100

    V 0.0.5

--- functions.py
+++ functions.py
@@ -24,6 +24,7 @@ def userLogin(email, password):
                 password = md5(password.encode('utf-8')).hexdigest()
                 password = md5((password+Data[1]).encode('utf-8')).hexdigest()
 
+                # Development
                 if Type == "Development":
                     print(Data[0])
                     print(Data[1])
@@ -31,15 +32,14 @@ def userLogin(email, password):
 
                 # Sprawdzanie czy hasła są sobie równe
                 if password == Data[0]:
-                    flash('Pomyślnie zalogowano!', 'success')
                     return True
-                else:
-                    flash('Proszę wprowadzić poprawne dane!', 'danger')
-            else:
-                flash('Podany E-Mail nie istnieje w naszej bazie!', 'danger')
+
             cursor.close()
+
+        # Error Log
         except Exception as Error:
             print("userLogin - MySQL Error")
             print("Error: " + str(Error))
-    else:
-        print("Prosze wprowadzić login oraz hasło!")
+
+
+def userRegister()
